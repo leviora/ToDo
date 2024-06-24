3c14bf6fba69647008a6985834f1d24a5d8a4810
leviora <leviora1993@gmail.com>
2024-06-24 15:44:17 +0200

    Header removed Added GlobalControllerAdvice

---
+++ demo/src/main/java/com/mazowiecka/demo/Controller/GlobalControllerAdvice.java
@@ -0,0 +1,21 @@
+package com.mazowiecka.demo.Controller;
+
+import com.mazowiecka.demo.Service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+@ControllerAdvice
+public class GlobalControllerAdvice {
+    private final TaskService taskService;
+    @Autowired
+    public GlobalControllerAdvice(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @ModelAttribute
+    public void addAttributes(Model model) {
+        model.addAttribute("uncompletedTasks", taskService.getAllUncompleteTasks());
+    }
+}
